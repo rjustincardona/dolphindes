6f20613d6283b91c307a8643759f1793f8e3d665
Alessio Amaolo <alessioamaolo@gmail.com>
2025-04-21 14:20:03 -0400

    QCQP implementation and documentation, supports getting the dualvalue of the QCQP. get_grad, get_hess, and penalty vectors are not yet supported.

--- dolphindes/cvxopt/qcqp.py
+++ dolphindes/cvxopt/qcqp.py
@@ -3,53 +3,292 @@ Dual Problem Interface
 
 """
 
-__all__ = ['SharedProjQCQP'] 
+__all__ = ['SparseSharedProjQCQP'] 
 
 import numpy as np 
 import scipy.sparse as sp
+import sksparse.cholmod 
 
-class SharedProjQCQP():
-    def __init__(self, A0, s, c, A1, semidefinite_P, projections_diags: np.ndarray):
+class SparseSharedProjQCQP():
+    """Represents a QCQP with a single type of constraint over projection regions.
+
+    Problem is 
+    max_x x^dagger A0 x + 2 Re (x^dagger s0) + c
+    s.t.  Re(x^dagger A1 P_j A_2 x + 2 Re (x^dagger A_2^dagger P_j^dagger s1) + c1) = 0
+
+    for a list of projection matrices P_j. 
+
+    Attributes
+    ----------
+    A0 : scipy.sparse.csc_array
+        The matrix A0 in the QCQP.
+    s0 : np.ndarray
+        The vector s in the QCQP.
+    c : float
+        The constant c in the QCQP.
+    A1 : scipy.sparse.csc_array
+        The matrix A1 in the QCQP.
+    A2 : scipy.sparse.csc_array
+        The matrix A2 in the QCQP.
+    s1 : np.ndarray
+        The vector s1 in the QCQP.
+    c1 : float
+        The constant c1 in the QCQP.
+    projections_diags : np.ndarray
+        The diagonal elements of the projection matrices P_j.  
+    verbose : float
+        The verbosity level for debugging and logging.
+    Achofac : sksparse.cholmod.Cholesky
+        The Cholesky factorization of the total A matrix, which is updated when needed.
+    current_point : tuple
+        The current dual solution, which is only updated when the dual problem is solved. Tuple of (dual value, gradient, hessian).
+    """
+    def __init__(self, A0: sp.csc_array, s0: np.ndarray, c: float, A1: sp.csc_array, A2: sp.csc_array, 
+                 s1: np.ndarray, c1: float, projections_diags: np.ndarray, verbose: float = 0):
         self.A0 = A0
-        self.s = s 
+        self.s0 = s0
+        self.s1 = s1
         self.c = c
         self.A1 = A1
-
-        self.semidefinite_P = semidefinite_P
+        self.A2 = A2
+        self.c1 = c1
+        self.verbose = verbose
         self.projections_diags = projections_diags
-
+        self.Achofac = None
         self.current_dual = None 
 
-    def get_dual(self, lags: np.array, get_grad: bool = False, get_hess: bool = False) -> None:
-        if get_hess:
-            raise NotImplementedError("Hessian not implemented yet")
-        if get_grad:
-            raise NotImplementedError("Gradient not implemented yet")
+        self._update_chofac()
+
+    def _Sym(self, A: sp.csc_array) -> sp.csc_array:
+        """Gets the symmetric part of A"""
+        return (A + A.T.conj()) / 2
+    
+    def _add_projectors(self, lags: np.ndarray) -> np.ndarray:
+        """Combine the lagrange multipliers and the projectors into a joint projector
+
+        Parameters
+        ----------
+        lags : np.ndarray
+            The lagrange multipliers for the projectors.
         
-        totalA1 = self.A0 + self.A1 @ (lags[0] * self.semidefinite_P + np.sum(lags[1:, np.newaxis] * self.projections_diags, axis=0))
-        xstar = np.ones(totalA1.shape[0])
-        dualval = self.c + xstar.conjugate() @ totalA1 @ xstar
+        Returns
+        -------
+        np.ndarray
+            The diagonal elements of the combined projector matrix sum_j lag[j] P_j 
+        """
+        return np.sum(lags[:, np.newaxis] * self.projections_diags, axis=0)
 
-        if not get_grad:
-            return dualval 
-        elif get_grad and not get_hess:
-            return dualval, None
-        elif get_grad and get_hess:
-            return dualval, None, None
-        else:
-            raise NotImplementedError("This should not happen")
+    def _get_total_A(self, P: sp.csc_array) -> sp.csc_array:
+        """Gets the total A matrix for the QCQP = A0 + sum_j lag[j] A1 P_j A2 given sum_j lag[j] P_j"""
+        return self.A0 + self._Sym(self.A1 @ P @ self.A2)
+    
+    def _get_total_S(self, P: sp.csc_array) -> np.ndarray:
+        """Gets the total S vector for the QCQP = s0 + sum_j lag[j] P_j^dagger s1 given P = sum_j lag[j] P_j"""
+        return self.s0 + self.A2.T.conj() @ P.T.conj() @ self.s1
 
-    def solve_current_dual_problem(self):
-        # define mineig, etc 
-        # Send it to an optimizer 
+    def _update_chofac(self):
+        """Updates the cholesky factorization of the total A
+        
+        Returns
+        -------
+        sksparse.cholmod.Cholesky
+            The updated cholesky factorization of the total A matrix.
+        """
+        random_lags = np.random.rand(len(self.projections_diags))
+        P = self._add_projectors(random_lags)
+        A = self._get_total_A(sp.csc_array(sp.diags(P, format='csc')))
+        if self.verbose > 1: print(f"analyzing A of format and shape {type(A)}, {A.shape} and # of nonzero elements '{A.count_nonzero()}")
+        self.Achofac = sksparse.cholmod.analyze(A)
+        return self.Achofac
+
+    def _get_xstar(self, A: sp.csc_array, S: np.ndarray, get_xgrad: bool = False):
+        """For a total A and S, solve for xstar using the cholesky factorization of A
+        
+        For a given set of Lagrange multipliers, which define A and S, the x_star that maximizes the Lagrangian 
+        (and therefore defines the dual) is given by A x_star = S. Since the symbolic Cholesky factorization 
+        of A is known, because the sparsity pattern of A is always known, this solution can be found very
+        efficiently. 
+
+        Returns
+        -------
+        Acho : sksparse.cholmod.Cholesky
+            The cholesky factorization of A.
+        x_star : np.ndarray
+            The solution x_star to the equation A x_star = S.
+        grad_x : np.ndarray
+            The gradient of the Lagrangian with respect to x_star, if requested (otherwise empty array)
+
+        """
+        Acho = self.Achofac.cholesky(A)
+        x_star = Acho.solve_A(S)
+        Pdiag_list = self.projections_diags
+        grad_x = np.zeros(Pdiag_list.shape[0])
+        
+        if get_xgrad:
+            # grad_x_mat is a matrix containing the vectors b that require solving Ax=b for each lagrange multiplier
+            # this is useful for the Hessian calculation 
+            grad_x_mat = -self.A1 @ (x_star[:, None] * Pdiag_list[None, :]) + (self.s1[:, None] * Pdiag_list[None, :])
+            grad_x = Acho.solve_A(grad_x_mat)
+        
+        return Acho, x_star, grad_x
+        
+    def get_dual(self, lags: np.ndarray, get_grad: bool = False, get_hess: bool = False, penalty_s: list = None) -> tuple[float, np.ndarray, np.ndarray]:
+        """Gets the dual value and the derivatives of the dual with respect to Lagrange multipliers.
+
+        Parameters
+        ----------
+        lags : np.ndarray
+            The lagrange multipliers for the projectors.
+        get_grad : bool, optional
+            Whether to return the gradient of the dual with respect to the lagrange multipliers. Default is False.
+        get_hess : bool, optional
+            Whether to return the Hessian of the dual with respect to the lagrange multipliers. Default is False.
+        penalty_s : list, optional
+            A list of penalty vectors for the PSD boundary. If provided, the dual value and gradients will include a penalty term. Default is None.
+        
+        Returns
+        -------
+        dualval : float
+            The value of the dual function.
+        grad : np.ndarray
+            The gradient of the dual function with respect to the lagrange multipliers, if requested.
+        hess : np.ndarray
+            The Hessian of the dual function with respect to the lagrange multipliers, if requested.
+        """
+        if get_hess: raise NotImplementedError("SparseSharedProjQCQP cannot return the Hessian yet. Use a first order method.")
+        
+        grad, hess = [], []
+
+        P_diag = self._add_projectors(lags)
+        P = sp.csc_array(sp.diags(P_diag))
+        A = self._get_total_A(P)
+        S = self._get_total_S(P)
+
+        Acho, xstar, grad_x = self._get_xstar(A, S, get_xgrad = get_hess) # grad_x is needed for calculating the hessian. xstar is sufficient for the gradient. 
+
+        dualval = self.c + np.real(xstar.conjugate() @ A @ xstar)
+
+        if get_grad: # This is grad_lambda (not grad_x)
+            # slow way 
+            grad = np.zeros(self.projections_diags.shape[0])
+            for i in range(len(grad)): 
+                # print(-np.real(xstar.conj() @ (self.projections_diags[i] * xstar)))
+                # print(2*np.real(xstar.conj() @ (self.projections_diags[i] * self.s1)))
+                grad[i] = -np.real(xstar.conj() @ (self.projections_diags[i] * xstar)) + 2*np.real(xstar.conj() @ (self.projections_diags[i] * self.s1))
+
+            # fast way
+            # Px = self.projections_diags * xstar[:, np.newaxis]  # Efficiently apply each projection to xstar
+            # grad = -np.real(xstar.conj() @ self.A1 @ Px) + 2 * np.real(self.s1.conj() @ Px)
+
+        # Boundary penalty for the PSD boundary 
+        if penalty_s is not None:
+            assert False
+            penalty_matrix = np.column_stack(penalty_s)
+            A_inv_penalty = Acho.solve_A(penalty_matrix)
+            dualval += np.sum(np.real(A_inv_penalty.conj() * penalty_matrix)) # multiplies columns with columns, sums all at once
+        
+            if get_grad: 
+                pass 
+                #P_ps = self.projections_diags * penalty_matrix
+
+        return dualval, grad, hess 
+
+    def solve_current_dual_problem(self, method: str) -> None:
+        """Solves the current dual problem using the specified method.
+
+        Parameters
+        ----------
+        method : str
+            The method to use for solving the dual problem. 'newton' or 'bfgs'
+        """
+        raise NotImplementedError("SparseSharedProjQCQP cannot solve the dual problem yet.")
+
+
+class DenseSharedProjQCQP():
+    """Represents a QCQP with a single type of constraint over projection regions.
+
+    Problem is 
+    max_x x^dagger A0 x + 2 Re (x^dagger s0) + c
+    s.t.  Re(x^dagger A1 P_j x + 2 Re (x^dagger P_j^dagger s1) + c) = 0
+
+    for a list of projection matrices P_j. 
+
+    Attributes
+    ----------
+    A0 : np.ndarray 
+        The matrix A0 in the QCQP.
+    s0 : np.ndarray
+        The vector s in the QCQP.
+    c : float
+        The constant c in the QCQP.
+    A1 : np.ndarray
+        The matrix A1 in the QCQP.
+    projections_diags : np.ndarray
+        The diagonal elements of the projection matrices P_j. The first one should always be one such that A0 + lambda A1 P_0 is positive semidefinite for sufficiently large constant lambda. 
+    """
+    def __init__(self, A0, s0, c, A1, s1, projections_diags: np.ndarray, verbose: float = 0):
+        self.A0 = A0
+        self.s0 = s0
+        self.s1 = s1
+        self.c = c
+        self.A1 = A1
+        self.verbose = verbose
+        self.projections_diags = projections_diags
+        self.current_dual = None 
+
+    def _add_projectors(self, lags: np.ndarray) -> np.ndarray:
+        """Combine the lagrange multipliers and the projectors into a joint projector
+
+        Parameters
+        ----------
+        lags : np.ndarray
+            The lagrange multipliers for the projectors.
+        
+        Returns
+        -------
+        np.ndarray
+            The diagonal elements of the combined projector matrix sum_j lag[j] P_j 
+        """
+        return np.sum(lags[:, np.newaxis] * self.projections_diags, axis=0)
+
+    def _get_total_A(self, P: sp.csc_array) -> sp.csc_array:
+        """Gets the total A matrix for the QCQP = A0 + sum_j lag[j] A1 P_j given sum_j lag[j] P_j"""
+        return self.A0 + self.A1 @ P
+    
+    def _get_total_S(self, P: sp.csc_array) -> np.ndarray:
+        """Gets the total S vector for the QCQP = s0 + sum_j lag[j] P_j^dagger s1 given P = sum_j lag[j] P_j"""
+        return self.s0 + P.T.conj() @ self.s1
+
+    def _get_xstar(self, A: sp.csc_array, S: np.ndarray, get_xgrad: bool = False):
+        """For a total A and S, solve for xstar using the cholesky factorization of A
+        
+        For a given set of Lagrange multipliers, which define A and S, the x_star that maximizes the Lagrangian 
+        (and therefore defines the dual) is given by A x_star = S. Since the symbolic Cholesky factorization 
+        of A is known, because the sparsity pattern of A is always known, this solution can be found very
+        efficiently. 
+
+        Returns
+        -------
+        Acho : sksparse.cholmod.Cholesky
+            The cholesky factorization of A.
+        x_star : np.ndarray
+            The solution x_star to the equation A x_star = S.
+        grad_x : np.ndarray
+            The gradient of the Lagrangian with respect to x_star, if requested (otherwise empty array)
+
+        """
+        pass 
+        
+    def get_dual(self, lags: np.ndarray, get_grad: bool = False, get_hess: bool = False, penalty_s: list = None) -> tuple[float, np.ndarray, np.ndarray]:
+        """Gets the dual value and the derivatives of the dual with respect to Lagrange multipliers.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        """
         pass 
 
-    def solve_gcd(self):
+    def solve_current_dual_problem(self, method: str) -> None:
         pass 
-
-class SparseSharedProjQCQP(SharedProjQCQP):
-    def __init__(self, A, s, c, Q, projections_diags: np.ndarray):
-        super().__init__(A, s, c, Q, projections_diags)
-
-    def solve_current_problem(self):
-        pass
--- tests/test_qcqp.py
+++ tests/test_qcqp.py
@@ -1,20 +1,121 @@
 import pytest
-from dolphindes.cvxopt import SharedProjQCQP
+from dolphindes.cvxopt import SparseSharedProjQCQP
 import numpy as np 
+import scipy.sparse as sp 
+import os 
+from pathlib import Path
 
-def test_shared_proj_qcqp_initialization():
-    # Initialize with dummy data
-    A0 = np.array([[1, 2], [2, 4]])
-    s = np.zeros(2)
-    c = 0
-    A1 = np.array([[1, 2], [2, 4]])
-    semidefinite_P = np.array([1, 0])
-    projections_diags = np.array([[1, 1], [-1, 1]])
+@pytest.fixture
+def data_dir():
+    """Return the path to the reference data directory."""
+    return Path(os.path.dirname(__file__)) / "reference_arrays" / "qcqp_example"
 
-    # Test initialization
-    spqcqp = SharedProjQCQP(A0, s, c, A1, semidefinite_P, projections_diags)
-    assert spqcqp is not None
 
-    dualval = spqcqp.get_dual(np.array([1, 1, 1]), get_grad=False, get_hess=False)
-    print(dualval)
-    assert dualval is not None
+# @pytest.fixture
+# def sparse_qcqp_instance():
+#     A0 = sp.csc_array(np.array([[1, 2], [2, 4]]))
+#     s0 = np.ones(2)
+#     c = 0
+#     A1 = sp.csc_array(np.array([[1, 2], [2, 4]]))
+#     s1 = np.ones(2)/2
+
+#     projections_diags = np.array([[1j, 1j], [-1+0.2j, 2-0.4j]])
+
+#     return SparseSharedProjQCQP(A0, s0, c, A1, s1, projections_diags)
+
+# @pytest.fixture
+# def sparse_qcqp_instance():
+#     A0 = sp.csc_array(np.array([
+#         [1.0, 2.0, 0.0, 0.0],
+#         [2.0, 5.0, 1.0, 0.0],
+#         [0.0, 1.0, 3.0, 0.5],
+#         [0.0, 0.0, 0.5, 4.0]
+#     ], dtype=complex))
+
+#     A1 = sp.csc_array(np.array([
+#         [4.0, 1.0, 0.0, 0.0],
+#         [1.0, 3.0, 1.0, 0.5],
+#         [0.0, 1.0, 2.0, 1.0],
+#         [0.0, 0.5, 1.0, 2.0]
+#     ], dtype=complex))
+
+#     s0 = np.ones(4)
+#     s1 = np.ones(4) / 2
+#     c = 0.2
+
+#     projections_diags = np.array([
+#         [1.0 + 1j, 0.2 + 0.2j, 0.3 + 0.3j, 0.4 + 0.4j],
+#         [0.2 - 0.2j, 1.5 + 0.5j, 0.6 + 0.1j, 0.0 + 0.2j],
+#         [0.3 - 0.3j, 0.6 - 0.1j, 2.0 + 0j, 0.8 + 0.3j],
+#         [0.4 - 0.4j, 0.0 - 0.2j, 0.8 - 0.3j, 1.0 + 0j]
+#     ], dtype=complex)
+
+#     return SparseSharedProjQCQP(A0, s0, c, A1, s1, projections_diags)
+
+# def test_projector_addition(sparse_qcqp_instance):
+#     assert sparse_qcqp_instance is not None
+
+#     lags = np.array([1.0, 2.0, 3.0, 4.0])
+#     combined_projector = sparse_qcqp_instance._add_projectors(lags)
+#     assert combined_projector is not None
+
+#     # Manually compute the reference P
+#     P_reference = (
+#         sparse_qcqp_instance.projections_diags[0] * lags[0] +
+#         sparse_qcqp_instance.projections_diags[1] * lags[1] +
+#         sparse_qcqp_instance.projections_diags[2] * lags[2] +
+#         sparse_qcqp_instance.projections_diags[3] * lags[3]
+#     )
+
+#     # Check if the computed projector matches the reference
+#     assert np.allclose(combined_projector, P_reference)
+
+# # Test get_dual and compare to dual values
+def test_get_dual(data_dir):
+    lags = np.load(data_dir / 'ldos_sparse_lags.npy', allow_pickle=True)
+
+    A0 = sp.csc_array(sp.load_npz(data_dir / 'ldos_sparse_A0.npz'))
+    A1 = sp.csc_array(sp.load_npz(data_dir / 'ldos_sparse_A1.npz'))
+    A2 = sp.csc_array(sp.load_npz(data_dir / 'ldos_sparse_A2.npz'))
+    s0 = np.load(data_dir / 'ldos_sparse_s0.npy', allow_pickle=True)
+    s1 = np.load(data_dir / 'ldos_sparse_s1.npy', allow_pickle=True)
+    projections_diags = np.load(data_dir / 'ldos_some_projections.npy', allow_pickle=True)
+    # Interleave the real and imaginary parts of the projections
+    real_projs = projections_diags * lags[0::2][:, np.newaxis]
+    imag_projs = -1j * projections_diags * lags[1::2][:, np.newaxis]
+    
+    # Create interleaved array
+    interleaved_projs = np.empty((real_projs.shape[0] + imag_projs.shape[0], projections_diags.shape[1]), dtype=complex)
+    interleaved_projs[0::2] = real_projs
+    interleaved_projs[1::2] = imag_projs
+    
+    projections_diags = interleaved_projs
+
+    c = 0.0 
+    c1 = 0.0 
+    lags = np.ones(len(projections_diags)) 
+
+    assert projections_diags.shape[0] == len(lags), "Number of projections does not match number of lags."
+
+    sparse_ldos_qcqp = SparseSharedProjQCQP(A0, s0, c, A1, A2, s1, c1, projections_diags)
+    combined_projector = sp.diags(sparse_ldos_qcqp._add_projectors(lags))
+
+    ref_totalA = sp.load_npz(data_dir / 'ldos_sparse_totalA.npz')
+    calc_totalA = sparse_ldos_qcqp._get_total_A(combined_projector)
+    assert sp.issparse(calc_totalA), "Total A should be a sparse matrix."
+    assert sp.csr_array(calc_totalA).shape == ref_totalA.shape, "Shape of calculated total A does not match reference."
+    assert np.allclose(calc_totalA.toarray(), ref_totalA.toarray()), "Calculated total A does not match reference total A."
+
+    dual, grad, hess = sparse_ldos_qcqp.get_dual(lags, get_grad=True)
+    # assert dual is not None
+    # assert np.isclose(dual, 12, atol=0.5)
+
+    print(dual)
+    print(grad)
+    # pass 
+    # # Load the reference dual values
+    # reference_dual = np.load('refernce_arrays/qcqp_example/ldos_sparse_dual.npy')
+
+    # # Check if the computed dual matches the reference
+    # assert np.allclose(dual, reference_dual, atol=1e-6), "Dual values do not match reference values."
+    
