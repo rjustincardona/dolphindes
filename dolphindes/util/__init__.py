e5b6cf4786237027027c5c64f2d038962e51ef19
Alessio Amaolo <alessioamaolo@gmail.com>
2025-07-10 22:11:23 -0700

    added geometry utils for plotting and mask making

--- dolphindes/util/__init__.py
+++ dolphindes/util/__init__.py
@@ -1,4 +1,7 @@
 from .class_utils import check_attributes
 from .math_utils import CRdot, Sym
 
-__all__ = ['check_attributes', 'CRdot', 'Sym']
+class geometry_utils:
+    from .geometry_utils import embed_vector
+
+__all__ = ['check_attributes', 'CRdot', 'Sym', 'geometry_utils']
